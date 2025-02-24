d78223eaefab6d7adffcd80f9667a8bbfba618a0
jorgef2001 <jorge.jurado.casado.al@iespoligonosur>
2025-02-24 14:07:47 +0100

    modificado

--- holamundo.php
+++ holamundo.php
@@ -5,6 +5,10 @@
 	</head>
 	<body>
 		<?php
+			$nombre = "Jorge";
+			$edad = 23;
+			$altura = 1.73;
+			echo "¡Hola, soy " .$nombre .", tengo ". $edad . " y mido ". $altura . "!<br>";
 			print("Hola mundo, desde IES Poligono Sur");
 		?>
 	</body>
