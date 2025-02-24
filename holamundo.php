9260bc5efb4d23cc4a38b7c06d8ff0fdc3734e06
jorgef2001 <jorge.jurado.casado.al@iespoligonosur>
2025-02-24 14:35:53 +0100

    modificado

--- holamundo.php
+++ holamundo.php
@@ -9,7 +9,22 @@
 			$edad = 23;
 			$altura = 1.73;
 			echo "¡Hola, soy " .$nombre .", tengo ". $edad . " y mido ". $altura . "!<br>";
-			print("Hola mundo, desde IES Poligono Sur");
+			print("Hola mundo, desde IES Poligono Sur<br>");
+			$x=TRUE;
+			$y=FALSE;
+			$z=($y OR $x);
+			echo $z ? "true" : "false";
+
+			$a=1234;
+			$b=-123;
+			$c=0123;
+			$d=1_234.567;
+
+			if($z=true) {
+				echo "<br>ES UN VALOR TRUE";
+			} else {
+				echo "<br>ES UN VALOR FALSE";
+			}
 		?>
 	</body>
 </html>
